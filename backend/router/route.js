a84b7202470b89145222a5ab2addc88b38972c3d
Audrey Aubouin <ca.haestie@gmail.com>
2024-08-17 19:47:03 +0200

    test mep

--- backend/router/route.js
+++ backend/router/route.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const router = express.Router()
 
-
 const { getTowns, getTown} = require("../controller/town")
 const { getMap } = require("../controller/weather")
 
