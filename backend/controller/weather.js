0b90b5443ed6751ac830f8ffdb16b3e9e28dc1e4
Audrey Aubouin <ca.haestie@gmail.com>
2024-08-17 22:26:47 +0200

    test mep

--- backend/controller/weather.js
+++ backend/controller/weather.js
@@ -2,35 +2,41 @@ const Weather = require('../model/weather')
 
 
 exports.getMap = async (req, res) => {
-  try {  
-    let data = '';
-    const request = await fetch('http://api.weatherapi.com/v1/forecast.json?key=c04fe2e1748e473da1181653243103&q=' + req.params.latitude + ',' + req.params.longitude + '&days=7', (response, err) => {
-      response.setEncoding('utf8');
-      response.on('data', (chunk) => {
-        data += chunk;
-      });
-      response.on('end', () => {
-        data = JSON.parse(data);
-        let weather = {};
-        weather = new Weather(
-          data.current.temp_c, 
-          data.current.is_day, 
-          data.current.text, 
-          data.current.icon, 
-          data.current.wind_mph, 
-          data.current.wind_dir, 
-          data.current.precip_mm,   
-          data.current.humidity, 
-          data.current.cloud, 
-          data.current.uv, 
-          data.forecast
-        )
-        response.send(JSON.stringify( weather))
-      }); 
-    });
-  } catch(err) {
-    console.log(err)
+
+    let data = ''
+    fetch('http://api.weatherapi.com/v1/forecast.json?key=c04fe2e1748e473da1181653243103?' + new URLSearchParams({
+      q: req.params.latitude + ',' + req.params.longitude ,
+      day: 7
+  }).toString(),  {
+      method: "GET", 
+      mode: "no-cors", 
+      cache: "no-cache", 
+      headers: {
+         'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: "follow", 
+      referrerPolicy: "no-referrer", 
+    }).then(response => response.json())
+    .then(data => {
+      data = JSON.parse(data);
+      let weather = {};
+      weather = new Weather(
+        data.current.temp_c, 
+        data.current.is_day, 
+        data.current.text, 
+        data.current.icon, 
+        data.current.wind_mph, 
+        data.current.wind_dir, 
+        data.current.precip_mm,   
+        data.current.humidity, 
+        data.current.cloud, 
+        data.current.uv, 
+        data.forecast
+      )
+      return weather;
+    })
+    .catch(err => console.error(err));
   }
   
-};
+
   
