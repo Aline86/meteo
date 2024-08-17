06621489fa66d1cb9bbe62e209587fc82be6a991
Audrey Aubouin <ca.haestie@gmail.com>
2024-08-17 20:45:40 +0200

    test mep

--- backend/functions/api.js
+++ backend/functions/api.js
@@ -1,6 +1,8 @@
 const express = require('express')
-const router = express.Router()
-
+const api = express()
+const router = Router();
+const cors = require('cors')
+const serverless = require("serverless-http");
 
 const { getTowns, getTown} = require("../controller/town")
 const { getMap } = require("../controller/weather")
@@ -8,5 +10,7 @@ const { getMap } = require("../controller/weather")
 router.get('/towns/:name', getTowns)
 router.get('/town/:name', getTown)
 router.get('/showmap/:latitude/:longitude', getMap)
+api.use(cors())
+api.use("/api/", router);
 
-module.exports = router
+export const handler = serverless(api);
