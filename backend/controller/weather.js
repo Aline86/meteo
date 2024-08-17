ad2bb3e93edb98b71c218b574cc24233e07a8631
Audrey Aubouin <ca.haestie@gmail.com>
2024-08-17 22:32:27 +0200

    test mep

--- backend/controller/weather.js
+++ backend/controller/weather.js
@@ -4,7 +4,7 @@ const Weather = require('../model/weather')
 exports.getMap = async (req, res) => {
 
     let data = ''
-    fetch('http://api.weatherapi.com/v1/forecast.json?key=c04fe2e1748e473da1181653243103?' + new URLSearchParams({
+  fetch('http://api.weatherapi.com/v1/forecast.json?key=c04fe2e1748e473da1181653243103&' + new URLSearchParams({
       q: req.params.latitude + ',' + req.params.longitude ,
       day: 7
   }).toString(),  {
