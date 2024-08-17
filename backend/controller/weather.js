320cfc4cefb65b85f5e61c6c72735255f31b880f
Audrey Aubouin <ca.haestie@gmail.com>
2024-08-17 23:06:36 +0200

    test mep

--- backend/controller/weather.js
+++ backend/controller/weather.js
@@ -24,7 +24,7 @@ exports.getMap = (req, res) => {
           data.forecast
         )*/
           //res.send(JSON.stringify( response))
-          res.send(data)
+          res.send(JSON.parse(data))
         });
     });
   
