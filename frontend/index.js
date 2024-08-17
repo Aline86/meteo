f444006b0ea79a2a2acbe6376dc47bc2703439b6
Audrey Aubouin <ca.haestie@gmail.com>
2024-08-17 20:10:56 +0200

    test mep

--- frontend/index.js
+++ frontend/index.js
@@ -47,13 +47,13 @@ class Weather {
         return div;
     }
     getTowns = async (name) => 
-        await fetch(`http://localhost:3000/towns/${name}`)
+        await fetch(`https://main-branch--meteobackend.netlify.app/towns/${name}`)
 
     getTown = async (name) => 
-        await fetch(`http://localhost:3000/town/${name}`)
+        await fetch(`https://main-branch--meteobackend.netlify.app/town/${name}`)
 
     getWeather = async (latitude, longitude) => 
-        await fetch(`http://localhost:3000/showmap/${latitude}/${longitude}`)
+        await fetch(`https://main-branch--meteobackend.netlify.app/showmap/${latitude}/${longitude}`)
 
     createWeather = (elem) => {
         let latitude = elem.latitude;
